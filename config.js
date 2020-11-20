e413e6a38510dbac64d428bcfd75011eec13b090
ruvice <andrewwee97@gmail.com>
2020-11-20 22:30:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: 'ouzDZoUSvVcmbGObo9MKLWNLU',
-    consumer_secret: 'fhqAWOAz3d3fW2KTIGZ8Bk7HmM6QxOAvoDIg5YznyrN5sKFtp2',
-    access_token: '239318065-ESXqnYuJ2FxgiduIGWhKbBLutzQ5u1IOVtPSQ48T',
-    access_token_secret: 'GlIpKDdRtYeBNRuQmkUCMfOPEthpYXjWxqWrqn3lpZioq'
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: ''
     }
