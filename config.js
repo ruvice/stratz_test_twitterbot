b50d4d4d10ef7f428ac651b44ab6d51b08899048
ruvice <andrewwee97@gmail.com>
2020-11-20 22:45:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: 'ouzDZoUSvVcmbGObo9MKLWNLU',
-    consumer_secret: 'fhqAWOAz3d3fW2KTIGZ8Bk7HmM6QxOAvoDIg5YznyrN5sKFtp2',
-    access_token: '239318065-ESXqnYuJ2FxgiduIGWhKbBLutzQ5u1IOVtPSQ48T',
-    access_token_secret: 'GlIpKDdRtYeBNRuQmkUCMfOPEthpYXjWxqWrqn3lpZioq'
+    consumer_key: 'insert key here',
+    consumer_secret: 'insert key here',
+    access_token: 'insert token here',
+    access_token_secret: 'insert token here'
 }
