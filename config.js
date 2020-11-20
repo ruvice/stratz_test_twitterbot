6884bb8566bd7205a318669c80d43a62cd27f296
Andrew <andrewwee97@gmail.com>
2020-11-20 22:31:55 +0800

    added gitignore

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ module.exports = {
     consumer_secret: 'fhqAWOAz3d3fW2KTIGZ8Bk7HmM6QxOAvoDIg5YznyrN5sKFtp2',
     access_token: '239318065-ESXqnYuJ2FxgiduIGWhKbBLutzQ5u1IOVtPSQ48T',
     access_token_secret: 'GlIpKDdRtYeBNRuQmkUCMfOPEthpYXjWxqWrqn3lpZioq'
-    }
+}
