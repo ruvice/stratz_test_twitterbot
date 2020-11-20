6199e27df5a73fd419d8e9efebe84d6c31677d3c
ruvice <andrewwee97@gmail.com>
2020-11-20 22:45:58 +0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: 'insert key here',
-    consumer_secret: 'insert key here',
-    access_token: 'insert token here',
-    access_token_secret: 'insert token here'
-}
